f8695080c7b7f491f964795ebb4d243578568183
Albert Bowden <albert@sunlightfoundation.com>
2016-08-14 01:51:55 -0400

    more json coming out. style sheet created. 2nd and js are bunk

---
+++ maps/counties/template/leaflate-boilerplate.js
@@ -0,0 +1,9 @@
+*{margin:0; padding:0; border:none;}
+.content{text-align:center;}
+#leaflet-map{
+  width:1000px; 
+  height:700px;
+}
+div.leaflet-map-box{outline:2px dotted #000; padding:30px; width:100%; position:relative}
+.leaflet-map-panel-box{width:200px; float:right;}
+div.leaflet-map-box ul{width:50px; margin:10px auto;}
